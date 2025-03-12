b14004a5ef13d0d133e6de034cb1e6beea7f2242
Ruben Luna <43586408+rubenaluna@users.noreply.github.com>
2025-03-12 16:48:03 -0500

    Segmentation Queries (#68)
    * Add segmentation queries and related types and inputs
    
    * Add segmentation queries

---
+++ java/src/main/java/io/worlds/api/model/SegmentationOptionsInput.java
@@ -0,0 +1,66 @@
+package io.worlds.api.model;
+
+
+/**
+ * ## Segmentation ###
+ */
+public class SegmentationOptionsInput implements java.io.Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private java.util.List<GeoJSONPointInput> points;
+    private java.util.List<GeoJSONMultiPolygonInput> regions;
+
+    public SegmentationOptionsInput() {
+    }
+
+    public SegmentationOptionsInput(java.util.List<GeoJSONPointInput> points, java.util.List<GeoJSONMultiPolygonInput> regions) {
+        this.points = points;
+        this.regions = regions;
+    }
+
+    public java.util.List<GeoJSONPointInput> getPoints() {
+        return points;
+    }
+    public void setPoints(java.util.List<GeoJSONPointInput> points) {
+        this.points = points;
+    }
+
+    public java.util.List<GeoJSONMultiPolygonInput> getRegions() {
+        return regions;
+    }
+    public void setRegions(java.util.List<GeoJSONMultiPolygonInput> regions) {
+        this.regions = regions;
+    }
+
+
+
+    public static SegmentationOptionsInput.Builder builder() {
+        return new SegmentationOptionsInput.Builder();
+    }
+
+    public static class Builder {
+
+        private java.util.List<GeoJSONPointInput> points;
+        private java.util.List<GeoJSONMultiPolygonInput> regions;
+
+        public Builder() {
+        }
+
+        public Builder setPoints(java.util.List<GeoJSONPointInput> points) {
+            this.points = points;
+            return this;
+        }
+
+        public Builder setRegions(java.util.List<GeoJSONMultiPolygonInput> regions) {
+            this.regions = regions;
+            return this;
+        }
+
+
+        public SegmentationOptionsInput build() {
+            return new SegmentationOptionsInput(points, regions);
+        }
+
+    }
+}
