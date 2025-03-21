79bc8249f81f19d56b3f9e40ed28c7e7e7ae3b50
rubenaluna <ruben.luna@worlds.io>
2025-03-21 10:36:20 -0500

    Generate sources

---
+++ java/src/main/java/io/worlds/api/model/UpdatePointOfInterestInput.java
@@ -0,0 +1,109 @@
+package io.worlds.api.model;
+
+
+public class UpdatePointOfInterestInput implements java.io.Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @jakarta.validation.constraints.NotNull
+    private String id;
+    private org.springframework.graphql.data.ArgumentValue<String> siteId = org.springframework.graphql.data.ArgumentValue.omitted();
+    private org.springframework.graphql.data.ArgumentValue<String> name = org.springframework.graphql.data.ArgumentValue.omitted();
+    private org.springframework.graphql.data.ArgumentValue<GeoJSONPoint> position = org.springframework.graphql.data.ArgumentValue.omitted();
+    private org.springframework.graphql.data.ArgumentValue<java.lang.Object> metadata = org.springframework.graphql.data.ArgumentValue.omitted();
+
+    public UpdatePointOfInterestInput() {
+    }
+
+    public UpdatePointOfInterestInput(String id, org.springframework.graphql.data.ArgumentValue<String> siteId, org.springframework.graphql.data.ArgumentValue<String> name, org.springframework.graphql.data.ArgumentValue<GeoJSONPoint> position, org.springframework.graphql.data.ArgumentValue<java.lang.Object> metadata) {
+        this.id = id;
+        this.siteId = siteId;
+        this.name = name;
+        this.position = position;
+        this.metadata = metadata;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public org.springframework.graphql.data.ArgumentValue<String> getSiteId() {
+        return siteId;
+    }
+    public void setSiteId(org.springframework.graphql.data.ArgumentValue<String> siteId) {
+        this.siteId = siteId;
+    }
+
+    public org.springframework.graphql.data.ArgumentValue<String> getName() {
+        return name;
+    }
+    public void setName(org.springframework.graphql.data.ArgumentValue<String> name) {
+        this.name = name;
+    }
+
+    public org.springframework.graphql.data.ArgumentValue<GeoJSONPoint> getPosition() {
+        return position;
+    }
+    public void setPosition(org.springframework.graphql.data.ArgumentValue<GeoJSONPoint> position) {
+        this.position = position;
+    }
+
+    public org.springframework.graphql.data.ArgumentValue<java.lang.Object> getMetadata() {
+        return metadata;
+    }
+    public void setMetadata(org.springframework.graphql.data.ArgumentValue<java.lang.Object> metadata) {
+        this.metadata = metadata;
+    }
+
+
+
+    public static UpdatePointOfInterestInput.Builder builder() {
+        return new UpdatePointOfInterestInput.Builder();
+    }
+
+    public static class Builder {
+
+        private String id;
+        private org.springframework.graphql.data.ArgumentValue<String> siteId = org.springframework.graphql.data.ArgumentValue.omitted();
+        private org.springframework.graphql.data.ArgumentValue<String> name = org.springframework.graphql.data.ArgumentValue.omitted();
+        private org.springframework.graphql.data.ArgumentValue<GeoJSONPoint> position = org.springframework.graphql.data.ArgumentValue.omitted();
+        private org.springframework.graphql.data.ArgumentValue<java.lang.Object> metadata = org.springframework.graphql.data.ArgumentValue.omitted();
+
+        public Builder() {
+        }
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setSiteId(org.springframework.graphql.data.ArgumentValue<String> siteId) {
+            this.siteId = siteId;
+            return this;
+        }
+
+        public Builder setName(org.springframework.graphql.data.ArgumentValue<String> name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setPosition(org.springframework.graphql.data.ArgumentValue<GeoJSONPoint> position) {
+            this.position = position;
+            return this;
+        }
+
+        public Builder setMetadata(org.springframework.graphql.data.ArgumentValue<java.lang.Object> metadata) {
+            this.metadata = metadata;
+            return this;
+        }
+
+
+        public UpdatePointOfInterestInput build() {
+            return new UpdatePointOfInterestInput(id, siteId, name, position, metadata);
+        }
+
+    }
+}
