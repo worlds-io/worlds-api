bcc21d56d1f99355d271e709528d07aca0cb8b0d
Jackson Allen <jackson.allen@worlds.io>
2025-06-18 10:35:05 -0500

    Revert "Add MessageStateInput"
    This reverts commit 0437992949d1eccbde6a067574c27b3dca64ef97.

--- java/src/main/java/io/worlds/api/model/FilterMessageStateInput.java
+++ java/src/main/java/io/worlds/api/model/FilterMessageStateInput.java
@@ -8,37 +8,37 @@ public class FilterMessageStateInput implements java.io.Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private org.springframework.graphql.data.ArgumentValue<MessageStateInput> eq = org.springframework.graphql.data.ArgumentValue.omitted();
-    private org.springframework.graphql.data.ArgumentValue<MessageStateInput> ne = org.springframework.graphql.data.ArgumentValue.omitted();
-    private java.util.List<MessageStateInput> in;
+    private org.springframework.graphql.data.ArgumentValue<MessageState> eq = org.springframework.graphql.data.ArgumentValue.omitted();
+    private org.springframework.graphql.data.ArgumentValue<MessageState> ne = org.springframework.graphql.data.ArgumentValue.omitted();
+    private java.util.List<MessageState> in;
 
     public FilterMessageStateInput() {
     }
 
-    public FilterMessageStateInput(org.springframework.graphql.data.ArgumentValue<MessageStateInput> eq, org.springframework.graphql.data.ArgumentValue<MessageStateInput> ne, java.util.List<MessageStateInput> in) {
+    public FilterMessageStateInput(org.springframework.graphql.data.ArgumentValue<MessageState> eq, org.springframework.graphql.data.ArgumentValue<MessageState> ne, java.util.List<MessageState> in) {
         this.eq = eq;
         this.ne = ne;
         this.in = in;
     }
 
-    public org.springframework.graphql.data.ArgumentValue<MessageStateInput> getEq() {
+    public org.springframework.graphql.data.ArgumentValue<MessageState> getEq() {
         return eq;
     }
-    public void setEq(org.springframework.graphql.data.ArgumentValue<MessageStateInput> eq) {
+    public void setEq(org.springframework.graphql.data.ArgumentValue<MessageState> eq) {
         this.eq = eq;
     }
 
-    public org.springframework.graphql.data.ArgumentValue<MessageStateInput> getNe() {
+    public org.springframework.graphql.data.ArgumentValue<MessageState> getNe() {
         return ne;
     }
-    public void setNe(org.springframework.graphql.data.ArgumentValue<MessageStateInput> ne) {
+    public void setNe(org.springframework.graphql.data.ArgumentValue<MessageState> ne) {
         this.ne = ne;
     }
 
-    public java.util.List<MessageStateInput> getIn() {
+    public java.util.List<MessageState> getIn() {
         return in;
     }
-    public void setIn(java.util.List<MessageStateInput> in) {
+    public void setIn(java.util.List<MessageState> in) {
         this.in = in;
     }
 
@@ -50,24 +50,24 @@ public class FilterMessageStateInput implements java.io.Serializable {
 
     public static class Builder {
 
-        private org.springframework.graphql.data.ArgumentValue<MessageStateInput> eq = org.springframework.graphql.data.ArgumentValue.omitted();
-        private org.springframework.graphql.data.ArgumentValue<MessageStateInput> ne = org.springframework.graphql.data.ArgumentValue.omitted();
-        private java.util.List<MessageStateInput> in;
+        private org.springframework.graphql.data.ArgumentValue<MessageState> eq = org.springframework.graphql.data.ArgumentValue.omitted();
+        private org.springframework.graphql.data.ArgumentValue<MessageState> ne = org.springframework.graphql.data.ArgumentValue.omitted();
+        private java.util.List<MessageState> in;
 
         public Builder() {
         }
 
-        public Builder setEq(org.springframework.graphql.data.ArgumentValue<MessageStateInput> eq) {
+        public Builder setEq(org.springframework.graphql.data.ArgumentValue<MessageState> eq) {
             this.eq = eq;
             return this;
         }
 
-        public Builder setNe(org.springframework.graphql.data.ArgumentValue<MessageStateInput> ne) {
+        public Builder setNe(org.springframework.graphql.data.ArgumentValue<MessageState> ne) {
             this.ne = ne;
             return this;
         }
 
-        public Builder setIn(java.util.List<MessageStateInput> in) {
+        public Builder setIn(java.util.List<MessageState> in) {
             this.in = in;
             return this;
         }
